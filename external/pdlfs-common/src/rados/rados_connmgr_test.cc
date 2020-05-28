ef0c5b352225e06e204832183ee98bcf50ae791d
Qing Zheng <zhengq@cs.cmu.edu>
2020-05-28 18:02:38 -0400

    update rados conn mgr test

--- external/pdlfs-common/src/rados/rados_connmgr_test.cc
+++ external/pdlfs-common/src/rados/rados_connmgr_test.cc
@@ -17,7 +17,7 @@
 
 // Parameters for ceph.
 namespace {
-const char* FLAHS_user_name = "client.admin";
+const char* FLAGS_user_name = "client.admin";
 const char* FLAGS_rados_cluster_name = "ceph";
 const char* FLAGS_conf = NULL;  // Use ceph defaults
 }  // namespace
@@ -33,7 +33,7 @@ TEST(RadosConnMgrTest, OpenAndClose) {
   RadosConnMgrOptions options;
   RadosConnMgr* const mgr = new RadosConnMgr(options);
   RadosConn* conn;
-  ASSERT_OK(mgr->OpenConn(FLAGS_rados_cluster_name, FLAHS_user_name, FLAGS_conf,
+  ASSERT_OK(mgr->OpenConn(FLAGS_rados_cluster_name, FLAGS_user_name, FLAGS_conf,
                           RadosConnOptions(), &conn));
   mgr->Release(conn);
   delete mgr;
@@ -42,18 +42,35 @@ TEST(RadosConnMgrTest, OpenAndClose) {
 }  // namespace rados
 }  // namespace pdlfs
 
+namespace {
+void PrintUsage() {
+  fprintf(stderr, "Use --cluster, --user, and --conf to conf test.\n");
+  exit(1);
+}
+
+void ParseArgs(int argc, char* argv[]) {
+  for (int i = 1; i < argc; ++i) {
+    ::pdlfs::Slice a = argv[i];
+    if (a.starts_with("--cluster=")) {
+      FLAGS_rados_cluster_name = argv[i] + strlen("--cluster=");
+      fprintf(stderr, "set cluster: %s\n", FLAGS_rados_cluster_name);
+    } else if (a.starts_with("--user=")) {
+      FLAGS_user_name = argv[i] + strlen("--user=");
+      fprintf(stderr, "set user: %s\n", FLAGS_user_name);
+    } else if (a.starts_with("--conf=")) {
+      FLAGS_conf = argv[i] + strlen("--conf=");
+      fprintf(stderr, "set conf :%s\n", FLAGS_conf);
+    } else {
+      PrintUsage();
+    }
+  }
+}
+
+}  // namespace
+
 int main(int argc, char* argv[]) {
   if (argc > 1) {
-    for (int i = 1; i < argc; ++i) {
-      ::pdlfs::Slice a = argv[i];
-      if (a.starts_with("--user=")) {
-        FLAHS_user_name = argv[i] + strlen("--user=");
-      } else if (a.starts_with("--cluster")) {
-        FLAGS_rados_cluster_name = argv[i] + strlen("--cluster=");
-      } else if (a.starts_with("--conf")) {
-        FLAGS_conf = argv[i] + strlen("--conf");
-      }
-    }
+    ParseArgs(argc, argv);
     return ::pdlfs::test::RunAllTests(&argc, &argv);
   } else {
     return 0;
